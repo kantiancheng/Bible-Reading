eccd99168b82d2ef6774573aa0373c837c829f3c
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-11-30 17:42:05 -0800

    Save

--- assets/JS/analyze_json.js
+++ assets/JS/analyze_json.js
@@ -3,13 +3,13 @@ function analyze_json(book, chapter, verse) {
     let Analyze_json;
     if (chapter == undefined) {
         Analyze_json = Bible[book].chapters;
-        closed.log("Since there are no valid chapters, the entire book will be returned directly.");
+        console.log("Since there are no valid chapters, the entire book will be returned directly.");
     } else if (verse == undefined) {
         Analyze_json = Bible[book].chapters[chapter];
-        closed.log("The entire chapter is returned because there is no valid verse.");
+        console.log("The entire chapter is returned because there is no valid verse.");
     } else {
         Analyze_json = Bible[book].chapters[chapter][verse];
-        closed.log("All information will be returned" + "chapter" + chapter + " ," + "verse" + verse + ".");
+        console.log("All information will be returned" + "chapter" + chapter + " ," + "verse" + verse + ".");
     }
     console.log("Successfully analyze Json file");
     return Analyze_json;
