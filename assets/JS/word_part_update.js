9912a6aa67176dac8f687b3ffdebd9c80fb83fc0
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-01 20:26:56 -0800

    Save

--- assets/JS/word_part_update.js
+++ assets/JS/word_part_update.js
@@ -1,14 +1,20 @@
 //文字区域dom更新
 function word_part_update(language, book, chapter, verse) {
     let word_part_element = document.getElementById("Words_Part");
+    let re_try_times = 5;
 
     function update_html() {
         try {
             word_part_element.innerHTML = analyze_json(book, chapter, verse);
             console.log("%cSuccessfully update the page", "font-weight:bold;");
         } catch {
-            console.log("%cfail to update the page, re-trying.....", "font-weight:bold;");
-            setTimeout(update_html(), 3000);
+            if (re_try_times <= 0) {
+                console.log("%cfail to update the page!!!pleas re-try later.", "font-weight:bold;");
+            } else {
+                console.log("%cfail to update the page, re-trying.....", "font-weight:bold;");
+                console.log("%c" + re_try_times + "/5", "font-weight:bold;");
+                setTimeout(update_html(), 3000);
+            }
         }
     }
 
