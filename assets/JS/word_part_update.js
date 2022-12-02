98fcb1ce2036059149c7126d25c60b998347036d
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-01 19:49:02 -0800

    Save

--- assets/JS/word_part_update.js
+++ assets/JS/word_part_update.js
@@ -1,7 +1,11 @@
 //文字区域dom更新
 function word_part_update(language, book, chapter, verse) {
     let word_part_element = document.getElementById("Words_Part");
-    get_json(language);
+    try {
+        get_json(language);
+    } catch (error) {
+        console.log("%clanguage code dose not find, or the network have problem.", "font-weight:bold;");
+    }
     word_part_element.innerHTML = analyze_json(book, chapter, verse);
     console.log("%cSuccessfully update the page", "font-weight:bold;");
 }
