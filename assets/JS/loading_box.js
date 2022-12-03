212257191f508a487106be578d755390fb8805fb
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-03 08:12:43 -0800

    Save

--- assets/JS/loading_box.js
+++ assets/JS/loading_box.js
@@ -1,5 +1,5 @@
 //加载框
-function loading_box() {
+function loading_box_onload() {
     let loading_box_HTML = document.getElementById("Body");
     let loding_box_el = document.createElement("div");
     loding_box_el.className = "Loading_box";
