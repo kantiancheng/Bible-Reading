911f16ea4b3c2b2846a86f5c01ec115a78b8b838
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-03 08:16:27 -0800

    Save

--- assets/JS/loading_box.js
+++ assets/JS/loading_box.js
@@ -2,6 +2,7 @@
 function loading_box_onload() {
     let loading_box_HTML = document.getElementById("Body");
     let loding_box_el = document.createElement("div");
+    loding_box_el.id= "Loading_box";
     loding_box_el.className = "Loading_box";
     loading_box_HTML.append(loding_box_el);
 }
@@ -9,4 +10,9 @@ function loading_box_onload() {
 //加载框更新
 function loading_box_onload(words) {
     
+}
+
+//加载框删除
+function loading_box_remove() {
+    document.getElementById("Loading_box").remove();
 }
