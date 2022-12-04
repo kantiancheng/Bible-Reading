7dc5c7c33058a2f4213df6bd23fc30495b1b4bf7
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-03 18:29:37 -0800

    Save

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -7,7 +7,7 @@ function window_load() {
             word_part_update("en_bbe", 0);
         } catch (error) {
             console.log("%cPage initialization failed with error:" + error, "font-weight:bold;");
-            console.log("%cStart retrying", "font-weight:bold;");
+            console.log("%cStart retrying...", "font-weight:bold;");
             setTimeout(() => {
                 window_load();
             }, 3000);
