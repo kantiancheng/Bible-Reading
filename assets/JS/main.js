f8cfea8f3e134b4c5c234dee396975a531f83e57
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-03 20:22:09 -0800

    Save

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,7 @@
 window.onload = window_load();
 
 function window_load() {
-    setTimeout(() => {
+   /* setTimeout(() => {
         try {
             loading_box_onload();
             word_part_update("en_bbe", 0);
@@ -13,5 +13,11 @@ function window_load() {
                 window_load();
             }, 3000);
         }
+    }, 3000);*/
+
+    setTimeout(() => {
+        loading_box_onload();
+        word_part_update("en_bbe", 0);
+        side_bar_click();
     }, 3000);
 } 
