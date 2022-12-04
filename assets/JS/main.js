1330f6e2a7d3bba7ee483934a9cef9351e7f271a
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-03 18:49:05 -0800

    Save

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,6 +5,7 @@ function window_load() {
         try {
             loading_box_onload();
             word_part_update("en_bbe", 0);
+            side_bar_click();
         } catch (error) {
             console.log("%cPage initialization failed with error:" + error, "font-weight:bold;");
             console.log("%cStart retrying...", "font-weight:bold;");
