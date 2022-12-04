5e887e0dcffa55b22b1a8b54cfdedfcc33e35afd
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-03 18:48:50 -0800

    Save

--- assets/JS/side_bar_click.js
+++ assets/JS/side_bar_click.js
@@ -1,4 +1,4 @@
 //为边框添加点击监听事件
 function side_bar_click(){
-    document.getElementById("").addEventListener("click", );
+    document.getElementById("Sides_Bar_Genesis").addEventListener("click", word_part_update("en_bbe", 0));
 }
