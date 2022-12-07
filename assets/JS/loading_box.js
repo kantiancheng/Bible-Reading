e54076361824edc192fdbabd46a908cfed77ea44
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-06 20:43:43 -0800

    Save

--- assets/JS/loading_box.js
+++ assets/JS/loading_box.js
@@ -1,18 +1,19 @@
 //加载框
 function loading_box_onload() {
-    document.getElementById("Body").style.backgroundColor="black";
     document.getElementById("Main_Box").style.display = "none";
     let loading_box_HTML = document.getElementById("Body");
-    let loading_box_el = document.createElement("div");
-    loading_box_el.id = "Loading_box";
-    loading_box_el.className = "Loading_box";
-    loading_box_HTML.append(loading_box_el);
+    let loading_box_Main = document.createElement("div");
+    loading_box_Main.id = "Loading_box";
+    loading_box_Main.className = "Loading_box";
+    let loading_box_Img = document.createElement("div");
+    loading_box_Img.id = "Loading_box_img";
+    let loading_box_Word = document.createElement("div");
+    loading_box_Word.id = "Loading_box_word";
+    loading_box_HTML.append(loading_box_Main);
 }
 
 //加载框更新
 function loading_box_update(words) {
-    document.getElementById("Body").style.backgroundColor="black";
-    document.getElementById("Main_Box").style.display = "none";
     document.getElementById("Loading_box").style.display = "inline";
     let loading_box_el = document.getElementById("Loading_box");
     let br_el = document.createElement("br");
@@ -24,7 +25,6 @@ function loading_box_update(words) {
 
 //加载框删除
 function loading_box_remove() {
-    document.getElementById("Body").style.backgroundColor="rgb(255, 255, 255)";
     document.getElementById("Loading_box").style.display = "none";
     document.getElementById("Main_Box").style.display = "inline";
 }
