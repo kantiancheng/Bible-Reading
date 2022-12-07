d70455a140543c58b5c666638385bc4f98a99324
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-06 20:46:36 -0800

    Save

--- assets/JS/loading_box.js
+++ assets/JS/loading_box.js
@@ -1,25 +1,26 @@
 //加载框
 function loading_box_onload() {
-    document.getElementById("Main_Box").style.display = "none";
     let loading_box_HTML = document.getElementById("Body");
+
     let loading_box_Main = document.createElement("div");
     loading_box_Main.id = "Loading_box";
     loading_box_Main.className = "Loading_box";
+
     let loading_box_Img = document.createElement("div");
     loading_box_Img.id = "Loading_box_img";
+
     let loading_box_Word = document.createElement("div");
     loading_box_Word.id = "Loading_box_word";
+
+    loading_box_Main.append(loading_box_Img);
+    loading_box_Main.append(loading_box_Word);
+
     loading_box_HTML.append(loading_box_Main);
 }
 
 //加载框更新
 function loading_box_update(words) {
-    document.getElementById("Loading_box").style.display = "inline";
-    let loading_box_el = document.getElementById("Loading_box");
-    let br_el = document.createElement("br");
-    loading_box_el.append(br_el);
-    loading_box_el.append(words);
-    loading_box_el.append(br_el);
+
     console.log("%c" + words, "font-weight:bold;");
 }
 
