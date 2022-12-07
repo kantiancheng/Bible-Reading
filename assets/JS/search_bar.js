cca4fed8ac5bef63d42ef45e8ea8b6dc61de6974
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-06 16:41:59 -0800

    Save

--- assets/JS/search_bar.js
+++ assets/JS/search_bar.js
@@ -6,5 +6,7 @@ function search_bar() {
     Search_Bar.Verse = document.getElementById("Search_Bar_Box_V").value;
     Search_bar.Language = document.getElementById("Search_Bar_Box_L").selectedIndex;
 
-let Language_JsonFileName = ['ar_svd', 'zh_cuv',' zh_ncv', 'de_schlachter' , 'el_greek', 'en_bbe','en_kjv', 'eo_esperanto' ,'es_rvr', 'fi_finnish', 'fi_pr', 'fr_apee', 'ko_ko', 'pt_aa', 'pt_acf', 'pt_nvi', 'ro_cornilescu', 'ru_synodal', 'vi_vietnamese'];
+    let Language_JsonFileName = ['ar_svd', 'zh_cuv', ' zh_ncv', 'de_schlachter', 'el_greek', 'en_bbe', 'en_kjv', 'eo_esperanto', 'es_rvr', 'fi_finnish', 'fi_pr', 'fr_apee', 'ko_ko', 'pt_aa', 'pt_acf', 'pt_nvi', 'ro_cornilescu', 'ru_synodal', 'vi_vietnamese'];
+
+    word_part_update(Language_JsonFileName[Search_bar.Language], Search_Bar.Chapter, Search_Bar.Verse);
 }
