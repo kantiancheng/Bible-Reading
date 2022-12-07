267088929d9c58daa22866c96d839ba497502f96
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-06 17:26:12 -0800

    Save

--- assets/JS/search_bar.js
+++ assets/JS/search_bar.js
@@ -4,7 +4,7 @@ function search_bar() {
     Search_Bar.Book = document.getElementById("Search_Bar_Box_B").selectedIndex;
     Search_Bar.Chapter = document.getElementById("Search_Bar_Box_C").value;
     Search_Bar.Verse = document.getElementById("Search_Bar_Box_V").value;
-    Search_bar.Language = document.getElementById("Search_Bar_Box_L").selectedIndex;
+    Search_Bar.Language = document.getElementById("Search_Bar_Box_L").selectedIndex;
 
     let Language_JsonFileName = ['ar_svd', 'zh_cuv', ' zh_ncv', 'de_schlachter', 'el_greek', 'en_bbe', 'en_kjv', 'eo_esperanto', 'es_rvr', 'fi_finnish', 'fi_pr', 'fr_apee', 'ko_ko', 'pt_aa', 'pt_acf', 'pt_nvi', 'ro_cornilescu', 'ru_synodal', 'vi_vietnamese'];
 
