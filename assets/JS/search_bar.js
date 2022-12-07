20b939738eab8a2bd72ce8c229dd420093a130a4
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-06 16:17:29 -0800

    Save

--- assets/JS/search_bar.js
+++ assets/JS/search_bar.js
@@ -1,4 +1,10 @@
 //顶部搜索栏
 function search_bar() {
+    let Search_Bar = new Object();
+    Search_Bar.Book = document.getElementById("Search_Bar_Box_B").selectedIndex;
+    Search_Bar.Chapter = document.getElementById("Search_Bar_Box_C").value;
+    Search_Bar.Verse = document.getElementById("Search_Bar_Box_V").value;
+    Search_bar.Language = document.getElementById("Search_Bar_Box_L").selectedIndex;
+
     
 }
